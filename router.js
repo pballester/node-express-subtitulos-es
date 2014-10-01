8d1b6ba79244c599485472c0cf82c2990ca9428d
pballester <pballester@users.noreply.github.com>
2014-10-01 22:01:35 +0200

    .

--- router.js
+++ router.js
@@ -2,10 +2,12 @@ var express = require('express'),
 	router = express.Router(),
 	index = require('./routes/index'),
 	show = require('./routes/show'),
-	test = require('./routes/test2');
+	test = require('./routes/test'),
+	test2 = require('./routes/test2');
 
 router.get('/', index.index);
 router.get('/show/:id/:lang', show.show);
 router.get('/test', test.test);
+router.get('/test2', test2.test);
 
 module.exports = router;
