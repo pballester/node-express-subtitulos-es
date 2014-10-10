d95bb815ddf0b47c6948fb1ef04640c21440f3a9
Pablo BC <pabbalca@gmail.com>
2014-10-10 21:21:30 +0200

    Services shown in home

--- routes/download.js
+++ routes/download.js
@@ -4,9 +4,7 @@ var tvShowDownloader = require("../modules/tvShowDownloader"),
 exports.download = function(req, res) {
 	tvShowInfo.getSubtitleFiles(req.params.id, req.params.season, req.params.lang, function(arrayFilesToDownload) {
 		if (arrayFilesToDownload === null || arrayFilesToDownload.length < 1) {
-			res.render('index', {
-				title: "No subtitles found"
-			});
+			res.status(500).send({ error: 'No subtitles found' });
 		}
 		tvShowDownloader.downloadSubtitleFiles(arrayFilesToDownload, "subitles.zip", res);
 	});
